ae9f139386cbc6aacf8decec7f5e29c6266f3831
Darjetii-Dooku <134135833+Darjetii-Dooku@users.noreply.github.com>
2023-10-26 12:30:32 +0000

    Deploying to gh-pages from @ Darjetii-Dooku/goit-js-9hw@80aa1e50db6489e72fab7bd3b42e803ab210ef37 🚀

---
+++ 01-color-switcher.87693584.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body");t.addEventListener("click",(function(d){var n=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3);t.disabled=!0,e.disabled=!1,e.addEventListener("click",(function(a){clearInterval(n),t.disabled=!1,e.disabled=!0}))}));e.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.87693584.js.map
